9ac2455f06e709df969bfea0ebd72494fe4391bb
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-06-09 23:51:59 +0400

    Add recursive call to find data locations

--- preprocess/convert.py
+++ preprocess/convert.py
@@ -8,8 +8,11 @@ from scipy.io import loadmat
 
 
 class Lookup:
-    def __init__(self, path='../data'):
-        self.path = path
+    def __init__(self, path='data', limit=3):
+        self.path = self.find_path(path, limit=limit)
+
+        # define extensions
+        self.extensions = ['.mat', '.dcm', '.txt', '.img', '.eeg']
 
         # empty lists to store found file extensions
         self.dcms = []              # Dicom files
@@ -24,18 +27,49 @@ class Lookup:
         # store directories
         self.dirs = []
 
+    def find_path(self, path, limit=3, idx=0):
+        """Recursively find path by traversing parent directory LIMIT times"""
+
+        # base case = raise error if not limit reached
+        if limit == idx:
+            raise ValueError(f'No folder {path} present in all {limit} levels.')
+
+        # return path if found
+        if os.path.exists(path):
+            return path
+
+        # update index and file location
+        idx += 1
+        path = os.path.join('', path)
+
+        # recursively call function
+        return self.find_path(path, idx=idx)
+
     def lookup(self):
         files = [x for x in os.listdir(self.path) if not x.startswith('.')]
 
         for file in files:
-            # path =
-            if os.path.isdir(os.path.join(self.path, file)):
+            path = os.path.join(self.path, file)
+            if os.path.isdir(path):
                 self.dirs.append(file)
-            # if
+            elif path[-4:] in self.extensions:
+                # append the file
+                self.files.append(path)
 
-        print(files)
-        print(self.dirs)
+                if path.endswith('.mat'):
+                    self.mats.append(path)
+                elif path.endswith('.dcm'):
+                    self.dcms.append(path)
+                elif path.endswith('.txt'):
+                    self.txts.append(path)
+                elif path.endswith('.eeg'):
+                    self.eegs.append(path)
+                elif path.endswith('.img'):
+                    self.imgs.append(path)
+
 
 
 look = Lookup()
 look.lookup()
+print(look.path)
+print(look.eegs, look.imgs, look.txts, look.mats, look.dcms)
