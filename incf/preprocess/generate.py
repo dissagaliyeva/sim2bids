078e5db1a348cc440eaeeac6d3755e466c00ed90
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-06-29 17:31:48 +0400

    Remove redundant lines

--- incf/preprocess/generate.py
+++ incf/preprocess/generate.py
@@ -28,23 +28,66 @@ def check_file(og_path, values, output='../output', save=False):
 
     layout = create_layout(subs, output)
 
-    # create_output_folder(output, subs)
-
     return layout
-    # sid = prep.create_uuid()
-    # fname = os.path.basename(abs_path)
-
-    # if fname.endswith('.txt'):
-    # TODO: cover weights.txt & distances.txt
-
-    # TODO: cover centres.txt
-
 
 # TODO: create function to determine separators
 
 
 def save_files(subs, output):
     pass
+
+
+def create_layout(subs=None, output='../output'):
+    """
+    Create folder structure according to BEP034 and passed `subs` parameter.
+
+    :param output:
+    :param subs:
+    :return:
+    """
+
+    output = output.replace('.', '').replace('/', '')
+    layout = '&emsp;'.join(create_sub(subs))
+
+    return f"""
+    {output}/ <br>
+        &emsp;|___ code <br>
+        &emsp;|___ coord <br>
+        &emsp;|___ eq <br>
+        &emsp;|___ param <br>
+        &emsp;{layout}
+        &emsp;|___ README <br>
+        &emsp;|___ CHANGES <br>
+        &emsp;|___ dataset_description.json <br>
+        &emsp;|___ participants.tsv
+    """
+
+
+def create_sub(subs):
+    outputs = []
+    for k, v in subs.items():
+        print(k, v, end='\n\n')
+
+        name = subs[k]['name'].split('.')[0]
+        if subs[k]['name'] == 'weights.txt':
+            outputs.append(f"""
+                    |___ sub-{subs[k]['sid']} <br>
+                        &emsp;&emsp;&emsp;|__ net <br>
+                            &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.tsv <br>
+                            &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.json <br>
+                        &emsp;&emsp;&emsp;|__ spatial <br>
+                        &emsp;&emsp;&emsp;|__ ts  <br>
+                    """)
+        elif subs[k]['name'] == 'distances.txt':
+            outputs.append(f"""
+                    |___ sub-{subs[k]['sid']} <br>
+                         &emsp;&emsp;&emsp;|__ net <br>
+                             &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.tsv <br>
+                             &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.json <br>
+                         &emsp;&emsp;&emsp;|__ spatial <br>
+                         &emsp;&emsp;&emsp;|__ ts  <br>
+                    """)
+    return outputs
 
 
 # def check_file(fname, content, path='../output', save=False):
@@ -134,77 +177,3 @@ def check_folders(path):
             print(f'Creating file `{os.path.basename(p)}`...')
             Path(p).touch()
 
-
-def create_layout(subs=None, output='../output'):
-    """
-    Create folder structure according to BEP034 and passed `subs` parameter.
-
-    :param output:
-    :param subs:
-    :return:
-    """
-
-    output = output.replace('.', '').replace('/', '')
-    layout = '&emsp;'.join(create_sub(subs))
-
-    return f"""
-    {output}/ <br>
-        &emsp;|___ code <br>
-        &emsp;|___ coord <br>
-        &emsp;|___ eq <br>
-        &emsp;|___ param <br>
-        &emsp;{layout}
-        &emsp;|___ README <br>
-        &emsp;|___ CHANGES <br>
-        &emsp;|___ dataset_description.json <br>
-        &emsp;|___ participants.tsv
-    """
-
-
-def create_sub(subs):
-    outputs = []
-    for k, v in subs.items():
-        print(k, v, end='\n\n')
-
-        name = subs[k]['name'].split('.')[0]
-        if subs[k]['name'] == 'weights.txt':
-            outputs.append(f"""
-                    |___ sub-{subs[k]['sid']} <br>
-                        &emsp;&emsp;&emsp;|__ net <br>
-                            &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.tsv <br>
-                            &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.json <br>
-                        &emsp;&emsp;&emsp;|__ spatial <br>
-                        &emsp;&emsp;&emsp;|__ ts  <br>
-                    """)
-        elif subs[k]['name'] == 'distances.txt':
-            outputs.append(f"""
-                    |___ sub-{subs[k]['sid']} <br>
-                         &emsp;&emsp;&emsp;|__ net <br>
-                             &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.tsv <br>
-                             &emsp;&emsp;&emsp;&emsp;|__ sub-{subs[k]['sid']}_desc-{subs[k]['desc']}_{name}.json <br>
-                         &emsp;&emsp;&emsp;|__ spatial <br>
-                         &emsp;&emsp;&emsp;|__ ts  <br>
-                    """)
-    return outputs
-
-    # if sub is not None:
-    #     if sub['name'] == 'weights.txt':
-    #         return f"""
-    #         |___ sub-{sub['name']} <br>
-    #                  &emsp;&emsp;&emsp;|__ net <br>
-    #                      &emsp;&emsp;&emsp;&emsp;|__ sub-{sub['sid']}_desc-{sub['desc']}_{sub['name']}.tsv <br>
-    #                      &emsp;&emsp;&emsp;&emsp;|__ sub-{sub['sid']}_desc-{sub['desc']}_{sub['name']}.json <br>
-    #                  &emsp;&emsp;&emsp;|__ spatial <br>
-    #                  &emsp;&emsp;&emsp;|__ ts  <br>
-    #         """
-    #
-    #     elif sub['fname'] == 'distances.txt':
-    #         return f"""
-    #             &emsp;|___ sub-{sub['name']} <br>
-    #                  &emsp;&emsp;|__ net <br>
-    #                      &emsp;&emsp;&emsp;|__ sub-{sub['sid']}_desc-{sub['desc']}_{sub['name']}.tsv <br>
-    #                      &emsp;&emsp;&emsp;|__ sub-{sub['sid']}_desc-{sub['desc']}_{sub['name']}.json <br>
-    #                  &emsp;&emsp;|__ spatial <br>
-    #                  &emsp;&emsp;|__ ts  <br>
-    #         """
-    return ''
