9d528d903a96bd6d21e7a6afd61b1f0da79aeb53
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-08-16 10:39:08 +0400

    Fix scrolling in user guide

--- incf/templates/user_guide.py
+++ incf/templates/user_guide.py
@@ -6,28 +6,167 @@ instructions there.
 
 From now on we assume the files are preprocessed and ready to be used. There are several steps you need to take:
 
-- Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
+<ul>
+<li>Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
 or click on any space **outside** of the input box where you provided your path. If you provided the path and pressed
 “Enter”, but no files are shown below on the left-hand side, make sure to do it once again and/or check your
-input folder to verify files exist.
+input folder to verify files exist.</li>
 
-- Select files you want to convert. You have three options:
-
-Go back one level and select the whole folder containing the files <br>
+<li>Select files you want to convert. You have three options:
+<ul>
+<li>Go back one level and select the whole folder containing the files <br></li>
 <img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/folder.gif" width="75%"/>
 
-Select all files one-by-one <br>
+<li>Select all files one-by-one <br></li>
 <img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/one-by-one.gif" width="75%"/>
 
-Select-scroll the files <br>
+<li>Select-scroll the files <br></li>
 <img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/select-scroll.gif" width="75%"/>
+</ul>
 
-- Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
+<li>Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
 The structure just shows the possible output. You might see some empty folders there, they will be removed when
 the structure gets generated. This conversion follows the BEP034 specification, therefore, we need to keep the
-structure as minimal as possible.
-
-
-
+structure as minimal as possible.</li>
+</ul>
 
 """
+
+preprocess = """
+Using the app was created to ease your computational simulations’ conversion! Before passing your files to the
+conversion tool, make sure to **run the preprocessing pipeline beforehand**. We know that manually renaming your files
+is too time-consuming and, let’s be honest, annoying! Please run the cell under “Preprocess files” and follow the
+instructions there.
+
+From now on we assume the files are preprocessed and ready to be used. There are several steps you need to take:
+
+<ul>
+<li>Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
+or click on any space **outside** of the input box where you provided your path. If you provided the path and pressed
+“Enter”, but no files are shown below on the left-hand side, make sure to do it once again and/or check your
+input folder to verify files exist.</li>
+
+<li>Select files you want to convert. You have three options:
+<ul>
+<li>Go back one level and select the whole folder containing the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/folder.gif" width="75%"/>
+
+<li>Select all files one-by-one <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/one-by-one.gif" width="75%"/>
+
+<li>Select-scroll the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/select-scroll.gif" width="75%"/>
+</ul>
+
+<li>Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
+The structure just shows the possible output. You might see some empty folders there, they will be removed when
+the structure gets generated. This conversion follows the BEP034 specification, therefore, we need to keep the
+structure as minimal as possible.</li>
+</ul>
+
+"""
+
+supported = """
+Using the app was created to ease your computational simulations’ conversion! Before passing your files to the
+conversion tool, make sure to **run the preprocessing pipeline beforehand**. We know that manually renaming your files
+is too time-consuming and, let’s be honest, annoying! Please run the cell under “Preprocess files” and follow the
+instructions there.
+
+From now on we assume the files are preprocessed and ready to be used. There are several steps you need to take:
+
+<ul>
+<li>Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
+or click on any space **outside** of the input box where you provided your path. If you provided the path and pressed
+“Enter”, but no files are shown below on the left-hand side, make sure to do it once again and/or check your
+input folder to verify files exist.</li>
+
+<li>Select files you want to convert. You have three options:
+<ul>
+<li>Go back one level and select the whole folder containing the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/folder.gif" width="75%"/>
+
+<li>Select all files one-by-one <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/one-by-one.gif" width="75%"/>
+
+<li>Select-scroll the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/select-scroll.gif" width="75%"/>
+</ul>
+
+<li>Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
+The structure just shows the possible output. You might see some empty folders there, they will be removed when
+the structure gets generated. This conversion follows the BEP034 specification, therefore, we need to keep the
+structure as minimal as possible.</li>
+</ul>
+
+"""
+
+
+functionality = """
+Using the app was created to ease your computational simulations’ conversion! Before passing your files to the
+conversion tool, make sure to **run the preprocessing pipeline beforehand**. We know that manually renaming your files
+is too time-consuming and, let’s be honest, annoying! Please run the cell under “Preprocess files” and follow the
+instructions there.
+
+From now on we assume the files are preprocessed and ready to be used. There are several steps you need to take:
+
+<ul>
+<li>Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
+or click on any space **outside** of the input box where you provided your path. If you provided the path and pressed
+“Enter”, but no files are shown below on the left-hand side, make sure to do it once again and/or check your
+input folder to verify files exist.</li>
+
+<li>Select files you want to convert. You have three options:
+<ul>
+<li>Go back one level and select the whole folder containing the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/folder.gif" width="75%"/>
+
+<li>Select all files one-by-one <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/one-by-one.gif" width="75%"/>
+
+<li>Select-scroll the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/select-scroll.gif" width="75%"/>
+</ul>
+
+<li>Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
+The structure just shows the possible output. You might see some empty folders there, they will be removed when
+the structure gets generated. This conversion follows the BEP034 specification, therefore, we need to keep the
+structure as minimal as possible.</li>
+</ul>
+
+"""
+
+
+bep034 = """
+Using the app was created to ease your computational simulations’ conversion! Before passing your files to the
+conversion tool, make sure to **run the preprocessing pipeline beforehand**. We know that manually renaming your files
+is too time-consuming and, let’s be honest, annoying! Please run the cell under “Preprocess files” and follow the
+instructions there.
+
+From now on we assume the files are preprocessed and ready to be used. There are several steps you need to take:
+
+<ul>
+<li>Go to the “Select Files” tab, provide a path to your files under “Insert Path”, and make sure to **press “Enter”**
+or click on any space **outside** of the input box where you provided your path. If you provided the path and pressed
+“Enter”, but no files are shown below on the left-hand side, make sure to do it once again and/or check your
+input folder to verify files exist.</li>
+
+<li>Select files you want to convert. You have three options:
+<ul>
+<li>Go back one level and select the whole folder containing the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/folder.gif" width="75%"/>
+
+<li>Select all files one-by-one <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/one-by-one.gif" width="75%"/>
+
+<li>Select-scroll the files <br></li>
+<img src="https://raw.githubusercontent.com/dissagaliyeva/incf/main/static/user_guide/select-scroll.gif" width="75%"/>
+</ul>
+
+<li>Check out the automatically generated folder structure. Don’t worry, no files are generated at this stage.
+The structure just shows the possible output. You might see some empty folders there, they will be removed when
+the structure gets generated. This conversion follows the BEP034 specification, therefore, we need to keep the
+structure as minimal as possible.</li>
+</ul>
+
+"""
+
