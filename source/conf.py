1daf3598e8623948f2f13a68cdbfec4b658e002e
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-08-16 14:35:40 +0400

    Add sphinx build

--- source/conf.py
+++ source/conf.py
@@ -79,7 +79,7 @@ release = '1.0.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -132,7 +132,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
