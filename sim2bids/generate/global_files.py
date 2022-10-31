83f68b6f906de7d51d0dae4bd91a9209906df445
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-10-31 11:31:23 +0400

    `FIX`: global JSON files

---
+++ sim2bids/generate/global_files.py
@@ -0,0 +1,99 @@
+import json
+import os.path
+from collections import OrderedDict
+
+import pandas as pd
+
+from sim2bids.app import app
+from sim2bids.templates import templates as temp
+
+
+FILE_DESCRIPTIONS = {
+    'CHANGES': 'None so far.',
+    'README': 'Simulation output{}.',
+    'BIDSVersion': 1.7
+}
+
+PARTICIPANTS_FIELDS = ['participant_id', 'species', 'age', 'sex', 'handedness', 'strain', 'strain_rrid']
+
+
+def add_global_files():
+    # create CHANGES file if not present
+    write_file('CHANGES', FILE_DESCRIPTIONS['CHANGES'])
+
+    # create README file if not present
+    if app.MODEL_NAME:
+        write_file('README', FILE_DESCRIPTIONS['README'].format(f' for {app.MODEL_NAME} model'))
+    else:
+        write_file('README', FILE_DESCRIPTIONS['README'].format(''))
+
+    # create dataset_description file if not present
+    dataset = get_components('dataset_description')
+    dataset['Name'] = app.DESC
+    dataset['BIDSVersion'] = FILE_DESCRIPTIONS['BIDSVersion']
+
+    if app.SoftwareCode != 'MISSING':
+        dataset['ReferencesAndLinks'].append(app.SoftwareCode)
+    if app.SoftwareRepository:
+        dataset['ReferencesAndLinks'].append(app.SoftwareRepository)
+
+    # save participants file
+    write_file('participants.json', dataset, use_json=True)
+
+    # create participants.[tsv|json] file if not present
+    check_participants()
+
+
+def write_file(file, content, use_json=False):
+    # check if it already exists
+    path = os.path.join(app.OUTPUT, file)
+    if not os.path.exists(path) and not os.path.exists(path + '.txt'):
+        with open(path, 'w') as f:
+            if use_json:
+                json.dump(content, f)
+            else:
+                f.write(content)
+
+
+def get_components(name):
+    template = OrderedDict()
+
+    for spec in ['required', 'recommend']:
+        template.update({x: '' for x in temp.struct[name][spec]})
+
+    if 'ReferencesAndLinks' in template.keys():
+        template['ReferencesAndLinks'] = []
+
+    return template
+
+
+def check_participants():
+    df, path = None, os.path.join(app.OUTPUT, 'participants.tsv')
+    json_file = OrderedDict()
+
+    if os.path.exists(path):
+        df = pd.read_csv(path, index_col=None, sep='\t')
+        columns = [x.lower().strip() for x in df.columns]
+
+        if 'sex' in columns:
+            json_file['sex'] = {
+                'Description': 'sex of the participant as reported by the participant',
+                'Levels': {
+                    'M': 'male',
+                    'F': 'female'
+                }
+            }
+
+        if 'handedness' in columns:
+            json_file['handedness'] = {
+                'Description': 'handedness of the participant as reported by the participant',
+                'Levels': {
+                    'left': 'left',
+                    'right': 'right'
+                }
+            }
+
+        with open(os.path.join(app.OUTPUT, 'participants.json'), 'w') as f:
+            json.dump(json_file, f)
+
+
